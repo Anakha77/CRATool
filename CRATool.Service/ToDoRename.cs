b623ccdc2530af0eab7c9742b63bb5dba8603e01
Sorlin Rémi <rsorlin@msn.com>
2016-09-30 23:56:03 +0200

    Add project files.

---
+++ CRATool.Service/ToDoRename.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using log4net;
+
+namespace CRATool.Service
+{
+    public class ToDoRename
+    {
+        public void Listening(TcpListener server, ILog log)
+        {
+            try
+            {
+                var serverSideClient = server.AcceptTcpClient();
+                var serverSideClientStream = serverSideClient.GetStream();
+                var serverSideClientStreamReader = new StreamReader(serverSideClientStream);
+                var serverSideClientStreamWriter = new StreamWriter(serverSideClientStream);
+
+                string inputLine;
+                var request = new StringBuilder();
+
+                while (!string.IsNullOrEmpty(inputLine = serverSideClientStreamReader.ReadLine()))
+                {
+                    request.AppendLine(inputLine);
+                }
+                log.Info($"RECIEVED REQUEST : {request}");
+
+                var response = GetResponsecontent(request);
+
+                serverSideClientStreamWriter.Write(response);
+                serverSideClientStreamWriter.Flush();
+
+                log.Info($"RESPONSE SENT : {response}");
+
+                serverSideClient.Close();
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+
+        private static string GetResponsecontent(StringBuilder request)
+        {
+            var response = new StringBuilder("HTTP/1.1 200 OK");
+            response.AppendLine();
+            response.AppendLine();
+            response.AppendLine("<!DOCTYPE HTML PUBLIC \" -//IETF//DTD HTML 2.0//EN\">");
+            response.AppendLine("<http>");
+            response.AppendLine("<head>");
+            response.AppendLine("</head>");
+            response.AppendLine("<body>");
+            response.AppendLine($"<h2>Uri={GetUri(request.ToString())}</h2>");
+            response.AppendLine("</body>");
+            response.AppendLine("</http>");
+            return response.ToString();
+        }
+
+        private static string GetUri(string request)
+        {
+            var byteArray = Encoding.UTF8.GetBytes(request);
+            var memStream = new MemoryStream(byteArray);
+            var streamReader = new StreamReader(memStream);
+            var requestLine = streamReader.ReadLine();
+            if (requestLine == null) return "";
+            var requestLineComponents = requestLine.Split(' ');
+            return requestLineComponents[1];
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using log4net;
+using log4net.Config;
+
+namespace CRATool.TestServer
+{
+    class Program
+    {
+        public static readonly ILog Log = LogManager.GetLogger(typeof (Program));
+
+        static void Main(string[] args)
+        {
+            var loggerConfigFileInfo = new FileInfo("log4net.config.xml");
+            XmlConfigurator.ConfigureAndWatch(loggerConfigFileInfo);
+
+            RunServer();
+
+            Console.ReadLine();
+        }
+
+        private static void RunServer()
+        {
+            var server = new TcpListener(IPAddress.Any, 7391);
+            var service = new Service.ToDoRename();
+
+            try
+            {
+                server.Start();
+
+                while (true)
+                {
+                    service.Listening(server, Log);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.Message, ex);
+            }
+            finally
+            {
+                server.Stop();
+                Log.Warn("SERVER STOP");
+            }
+        }
+    }
+}
