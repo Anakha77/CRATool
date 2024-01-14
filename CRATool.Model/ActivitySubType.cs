63f82073f4f498369028f565c13dd96744a307ed
Anakha77 <rsorlin@msn.com>
2024-01-14 17:09:34 +0100

    move names from fr to en (#4)
    Co-authored-by: Remi Sorlin <rsorlin@rsorlin.fr>

---
+++ CRATool.Model/ActivitySubType.cs
@@ -0,0 +1,10 @@
+﻿namespace CRATool.Model
+{
+    public class ActivitySubType
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public bool IsProject { get; set; }
+        public ActivityType ActivityType { get; set; }
+    }
+}
